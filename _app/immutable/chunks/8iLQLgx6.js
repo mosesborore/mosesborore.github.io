892c1a4547e49b714fa1adf8ef12e21242212f37
Moses Borore <mosesbororekinuthia@gmail.com>
2026-02-01 12:50:22 +0300

    Updates

---
+++ _app/immutable/chunks/8iLQLgx6.js
@@ -0,0 +1,1 @@
+import{f as a}from"./Cmm229lX.js";a();
---
+++ _app/immutable/chunks/D8BlURLp.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_s7gv4s?.base??"/mosesborore.github.io",e=globalThis.__sveltekit_s7gv4s?.assets??s??"";export{e as a,s as b};
